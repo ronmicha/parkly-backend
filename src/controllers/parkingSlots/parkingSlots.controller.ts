4b2ddb1e707360c34e636de0530451f495c9092b
Ron Michaeli <ron.michaeli@viz.ai>
2023-07-13 14:35:10 +0300

    Parking slot management

--- src/controllers/parkingSlots/parkingSlots.controller.ts
+++ src/controllers/parkingSlots/parkingSlots.controller.ts
@@ -1,5 +1,10 @@
 import { QueryBuilder } from "../../db";
-import { DB_ParkingArea, DB_ParkingSlot, DB_UserVehicles } from "../../models";
+import {
+  DB_ParkingArea,
+  DB_ParkingSlot,
+  DB_User,
+  DB_UserVehicles,
+} from "../../models";
 
 const tableName = "parking_slots";
 
@@ -24,8 +29,25 @@ export const updateSlotStatus = async (
   slotId: DB_ParkingSlot["id"],
   vehicleId: DB_UserVehicles["vehicle_id"] | null
 ): Promise<void> => {
-  await new QueryBuilder()
-    .update({ tableName, data: { vehicle_id: vehicleId } })
-    .where({ id: slotId })
-    .execute<DB_ParkingSlot[]>();
+  await QueryBuilder.transaction<DB_User>(async (trx) => {
+    const rows = await new QueryBuilder(trx)
+      .select("*")
+      .from(tableName)
+      .where({ id: slotId })
+      .execute<DB_ParkingSlot[]>();
+
+    const slot: DB_ParkingSlot = rows[0];
+    const existingVehicleId = slot.vehicle_id;
+
+    if (vehicleId && existingVehicleId) {
+      throw new Error(
+        `Slot status could not be changed. This slot is already taken by vehicle '${existingVehicleId}'`
+      );
+    }
+
+    await new QueryBuilder(trx)
+      .update({ tableName, data: { vehicle_id: vehicleId } })
+      .where({ id: slotId })
+      .execute<DB_ParkingSlot[]>();
+  });
 };
